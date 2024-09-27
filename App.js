35beb5b742ba49c1b024f8145a5051d1984efe62
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-27 16:45:09 -0400

    Part 1  done return weatherCard for each item in the array

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React from "react";
 // Import data and WeatherCard here
-
-
+import WeatherCard from "./components/WeatherCard";
+const cities = require("./data")
+console.log(cities)
 function App() {
-    return (
-        <>
-            <h1 className = "title">REACTIVE WEATHER</h1>
-            <h3 className = "subtitle">Up to the minute weather news</h3>
-            <div className = "app">
-                {/* Render components here */}
-                
-
-
-            </div>
-            
-        </>
-    )
+  return (
+    <>
+      <h1 className="title">REACTIVE WEATHER</h1>
+      <h3 className="subtitle">Up to the minute weather news</h3>
+      <div className="app">
+        {
+          /* Render components here */
+          cities.map((city) => (
+            <WeatherCard  data={city} />
+          ))
+        }
+      </div>
+    </>
+  );
 }
 
 export default App;
