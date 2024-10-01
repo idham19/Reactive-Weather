d37af2a7b8ba1d0bfab4184b50642f6f573ce924
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-30 21:50:55 -0400

    compenent Location added

--- App.js
+++ App.js
@@ -2,22 +2,16 @@ import React, { useState } from "react";
 // Import data and WeatherCard here
 import WeatherCard from "./components/WeatherCard";
 const cities = require("./data");
-
-
+import Location from "./components/Location";
 
 function App() {
-  const [location,setLocation]=useState("")
+  const [location, setLocation] = useState("London");
   return (
     <>
       <h1 className="title">REACTIVE WEATHER</h1>
       <h3 className="subtitle">Up to the minute weather news</h3>
       <div className="app">
-        {
-          /* Render components here */
-          cities.map((city) => (
-            <WeatherCard data={city} />
-          ))
-        }
+        <Location data={cities} location={location} setLocation={setLocation} />
       </div>
     </>
   );
--- components/Location.js
+++ components/Location.js
@@ -1,9 +1,8 @@
-import React from "react";
+import app from "../App";
 import sunny from "../assets/Sunny.svg";
 import rainy from "../assets/Rainy.svg";
 import partlyCloudy from "../assets/PartlyCloudy.svg";
 import cloudy from "../assets/cloudy.svg";
-
 function weatherForcast(forecast) {
   return forecast === "Sunny"
     ? sunny
@@ -17,25 +16,31 @@ function weatherForcast(forecast) {
     ? rainy
     : null;
 }
-function WeatherCard({ data }) {
+
+function Location({ data, location, setLocation }) {
+  const cityData = data.find((city) => city.city === location);
+
   return (
     <div className="card">
+      <div>
+        <h2>Your Location's weather</h2>
+      </div>
       <div className="img-container">
         <img
           className="card-img-top"
-          src={weatherForcast(data.forecast)}
+          src={weatherForcast(cityData.forecast)}
           alt="Card image cap"
           id="icon"
         />
       </div>
       <div class="card-body">
-        <h3 className="card-title">{data.city}</h3>
-        <h5 className="card-text">{data.temperature}</h5>
-        <h5 className="card-text">{data.forecast}</h5>
+        {/* <h3 className="card-title">{data.city}</h3> */}
+        <h5 className="card-text">
+          The weather in {cityData.city} is {cityData.temperature}
+        </h5>
+        <h5 className="card-text"> It is {cityData.forecast} out today</h5>
       </div>
     </div>
   );
 }
-
-// Export the WeatherCard
-export default WeatherCard;
+export default Location;
